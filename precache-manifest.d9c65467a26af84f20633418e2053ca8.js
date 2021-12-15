ed3f2de8b522ca2976e549c397f0a97caec273fd
tongZhezhe <9807671+tongzhezhe@user.noreply.gitee.com>
2021-12-15 19:30:56 +0800

    deploy

---
+++ precache-manifest.d9c65467a26af84f20633418e2053ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13288b61bcb3c7d33638",
+    "url": "/more-money-website/css/app.02a86993.css"
+  },
+  {
+    "revision": "7b99acdade12ab06708243f13e8b0260",
+    "url": "/more-money-website/index.html"
+  },
+  {
+    "revision": "13288b61bcb3c7d33638",
+    "url": "/more-money-website/js/app.90cca69d.js"
+  },
+  {
+    "revision": "6926624231466d3b88d4",
+    "url": "/more-money-website/js/chunk-vendors.822b7e69.js"
+  },
+  {
+    "revision": "42bfa4ba9edfb3426e53b3524294dc7f",
+    "url": "/more-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/more-money-website/robots.txt"
+  }
+]);
